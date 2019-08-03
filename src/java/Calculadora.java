0187d3e252a8d327ba30b35b819a0ded5fcf0d7d
Jgarciam31 <jose123.garciamadrid@gmail.com>
2019-08-03 10:58:52 -0600

    Agregamos clase, programamos en Objetos, primera prueba

---
+++ src/java/Calculadora.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Jose1
+ */
+public class Calculadora {
+    
+public String nombreDeClase;
+private int resultadoSuma;
+    
+    //Metodo Publico de sumar
+    public int sumar(int firtnumber, int secondnumber){
+    int resultadoSuma = firtnumber+secondnumber;
+    return resultadoSuma;
+    }
+
+}
